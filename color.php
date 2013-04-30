8b5a5b73d4df2e8e761ad09eb2504ab2cc95d1a8
Nicholas Jordon <Admin@ProjectCleverWeb.com>
2013-04-30 02:15:05 -0400

    works on windows given ANSICON is installed

--- color.php
+++ color.php
@@ -78,31 +78,6 @@ fwrite(STDOUT, $output);
 exit();
 
 
-function colorize($text, $status) {
- $out = "";
- switch($status) {
-  case "SUCCESS":
-   $out = "[42m"; //Green background
-   break;
-  case "FAILURE":
-   $out = "[41m"; //Red background
-   break;
-  case "WARNING":
-   $out = "[43m"; //Yellow background
-   break;
-  case "NOTE":
-   $out = "[44m"; //Blue background
-   break;
-  default:
-   throw new Exception("Invalid status: " . $status);
- }
- // return sprintf("%c".$out.$text."%c[0m", 27);
- return chr(27) . "$out" . "$text" . chr(27) . "[0m";
-}
- 
-echo colorize("Your command has successfully executed...", "SUCCESS");
-
-
 // print all the colors
 // foreach ($color->get_txtcolors() as $value) {
 // 	echo $color->string('This is the txt color: '.str_replace('_', ' ',$value), $value).PHP_EOL;
